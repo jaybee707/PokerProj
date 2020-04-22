f47ebaa45d059e266163e578d4999c2c002038e7
Jesse Bond <jesse.o.bond@gmail.com>
2020-04-22 15:49:34 -0700

    updating game.java file

--- game.java
+++ game.java
@@ -1,16 +1,18 @@
 import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Random;
 
 public class Game{
     private boolean winner;
-    private LinkedList<PlayerInGame> players;
-    private Deck deck;
+    private static LinkedList<PlayerInGame> players;
+    private static Deck dealer;
     private LinkedList<card> community;
     private int mainPot;
     private int dealerButton;
 
     public Game(){
         //runs the game until winner == true
+        dealer = new Deck();
         winner = false;
         Random rand = new Random();
         dealerButton = rand.nextInt(3); //pick dealer button start location
@@ -20,12 +22,23 @@ public class Game{
             
             //based off round winner
             //give player winnings
-
+            ListIterator win = players.listIterator(roundWinner);
+            PlayerInGame won = win.next();
+            won.addChips(mainPot);
             //reset pot
+            mainPot = 0;
             //reset deck
+            dealer.resetDeck();
             //move dealer button
+            if(dealerButton == (players.size() - 1)){
+                dealerButton = 0;
+            }
+            else{
+                dealerButton++;
+            }
 
             //check for tourney winner
+            winner = tourneyWin();
         }
 
     }
@@ -33,6 +46,7 @@ public class Game{
     private static void deal(){
         /****test cases to make sure cards are coming out in order to the right players***/
         //function for dealing players their cards
+
     }
 
     private static void preflop(){
@@ -62,7 +76,7 @@ public class Game{
     }
 
     private static int round(){
-        //shuffle deck
+        dealer.shuffleDeck();
         deal();
         if(handwon()){
             //return int of winning player
@@ -86,6 +100,15 @@ public class Game{
         return showdown();
         /***test case to make sure proper int is being sent back****/
         /***test case to make sure money goes to correct player****/
+    }
+
+    private static boolean tourneyWin(){
+        if(players.size() == 1){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
 }
