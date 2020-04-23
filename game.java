e19fea55c5d43444a3250dd3eaf5ab8290d66e6f
Jesse Bond <jesse.o.bond@gmail.com>
2020-04-23 01:40:07 -0700

    saving updates to the game.java file

--- game.java
+++ game.java
@@ -15,27 +15,27 @@ public class Game{
         dealer = new Deck();
         winner = false;
         Random rand = new Random();
-        dealerButton = rand.nextInt(3); //pick dealer button start location
+        dealerButton = rand.nextInt(players.size()); //pick dealer button start location
 
         while(winner == false){         //*****winner being t/f needs to be a test case*****/
             int roundWinner = round();
             
             //based off round winner
             //give player winnings
-            ListIterator win = players.listIterator(roundWinner);
-            PlayerInGame won = win.next();
-            won.addChips(mainPot);
+            //ListIterator win = players.listIterator(roundWinner);
+            //PlayerInGame won = win.next();
+            //won.addChips(mainPot);
             //reset pot
-            mainPot = 0;
+            //mainPot = 0;
             //reset deck
-            dealer.resetDeck();
+            //dealer.resetDeck();
             //move dealer button
-            if(dealerButton == (players.size() - 1)){
+            /*if(dealerButton == (players.size() - 1)){
                 dealerButton = 0;
             }
             else{
                 dealerButton++;
-            }
+            }*/
 
             //check for tourney winner
             winner = tourneyWin();
@@ -43,72 +43,87 @@ public class Game{
 
     }
 
-    private static void deal(){
+    private static void deal(LinkedList<PlayerInGame> inHand){
         /****test cases to make sure cards are coming out in order to the right players***/
         //function for dealing players their cards
+        System.out.println("deals cards");
+        
 
     }
 
-    private static void preflop(){
+    private static void preflop(LinkedList<PlayerInGame> inHand){
         /****test cases to make sure chip counts and pots are properly updated applies to flop, turn, etc***/
         //function for preflop betting
+        System.out.println("starting with the player on the button, players take turns betting %n");
+        System.out.println("round of betting ends when everyone matches highest amount bet or folds %n");
+
     }
 
-    private static void flop(){
+    private static void flop(LinkedList<PlayerInGame> inHand){
         //function for flop betting
+        System.out.println("dealer adds three cards to community pool %n");
+        System.out.println("another round of betting %n");
     }
 
-    private static void turn(){
+    private static void turn(LinkedList<PlayerInGame> inHand){
         //function for turn betting
+        System.out.println("dealer adds a fourth card to community pool %n");
+        System.out.println("another round of betting %n");
     }
 
-    private static void river(){
+    private static void river(LinkedList<PlayerInGame> inHand){
         //function for river betting
+        System.out.println("dealer adds a fifth card to community pool %n");
+        System.out.println("another round of betting %n");
     }
 
-    private static int showdown(){
+    private static int showdown(LinkedList<PlayerInGame> inHand){
         //compares hands to see who wins
+        System.out.println("compares hands of players still in hand %n");
+        System.out.println("adds pot to winning players chip pool %n");
     }
 
     private static boolean handWon(){
         /*****test cases for only one player left in hand or multiple****/
         //checks for winner before showdown
+        System.out.println("if only one player left hand is over");
+
     }
 
     private static int round(){
         dealer.shuffleDeck();
+        LinkedList<PlayerInGame> playersInRound;
         deal();
-        if(handwon()){
-            //return int of winning player
-        }
+        
         preflop();
         if(handwon()){
             //return int of winning player
         }
         flop();
-        if(handwon()){
+        //if(handwon()){
             //return int of winning player
-        }
+        //}
         turn();
-        if(handwon()){
+        //if(handwon()){
             //return int of winning player
-        }
+        //}
         river();
-        if(handwon()){
+        //if(handwon()){
             //return int of winning player
-        }
+        //}
         return showdown();
         /***test case to make sure proper int is being sent back****/
         /***test case to make sure money goes to correct player****/
     }
 
     private static boolean tourneyWin(){
-        if(players.size() == 1){
+        /*if(players.size() == 1){
             return true;
         }
         else{
             return false;
-        }
+        }*/
+        return true;
     }
 
 }
