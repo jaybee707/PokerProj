3c6934557361f0048028388f778a1ecc6de9323c
Jesse Bond <jesse.o.bond@gmail.com>
2020-05-10 16:28:39 -0700

    fleshed out game driver and added functions to the playeringame class

--- PlayerInGame.java
+++ PlayerInGame.java
@@ -3,7 +3,10 @@ import java.util.LinkedList;
 public class PlayerInGame {
 	private LinkedList<Card> twoCardHand;
 	private LinkedList<Card> fiveCardHand;
+	private int handRank;
 	private int chipCount;
+	private int totalInvest;
+	private boolean inHand;
 
 	// Constructor: Sets chipCount and the two card hand the player has
 	public PlayerInGame(/*LinkedList<Card> playerHand,*/ int count){
@@ -36,6 +39,8 @@ public class PlayerInGame {
 	public void setBestFiveCardHand(LinkedList<Card> hand) {
 		// Takes in the community 5 card hand as a paramater and sets the players fiveCardHand to
 		// the best 5 card hand the player can have using the 2 cards hand they currently have
+		LinkedList<Card> allCards = new LinkedList<Card>();
+
 	}
 
 	//
@@ -49,18 +54,43 @@ public class PlayerInGame {
 	}
 
 	//
-	public void hold() {
+	public void fold() {
 
 	}
 
 	//
-	public void bet() {
+	public int bet(int biggestBet) {
 
 	}
 
 	//
-	public void call() {
+	public int call(int biggestBet) {
 
 	}
 
+	public int blind(int amount){
+		chipCount -= amount;
+		totalInvest += amount;
+		return amount;
+	}
+
+	public int getTotalInvest(){
+		return totalInvest;
+	}
+
+	public void resetTotalInvest(){
+		totalInvest = 0;
+	}
+
+	public boolean getInHand(){
+		return inHand;
+	}
+		
+	public void setInHand(boolean a){
+		inHand = a;
+	}	
+
+    public int getHandRank(){
+		return handRank;
+	}
 }
